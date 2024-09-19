f85d034d35ba01fa7aa6c7a9f28b688b1a96bdb4
flyBoy667 <salifoudembele100@outlook.com>
2024-09-19 14:28:41 +0000

    Config models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+
+    # auto_now sera applique a chaque sauvegarde alors que auto_now_add uniquement au moment de l'enregistrement
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    body = models.TextField()
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.body[0:50])
--- base/views.py
+++ base/views.py
@@ -1,31 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from base.models import Room
 
-rooms = [
-    {
-        'id': 1,
-        'name': 'Lets learn python'
-    },
-    {
-        'id': 2,
-        'name': 'Design with me'
-    },
-    {
-        'id': 3,
-        'name': 'Fronted devs'
-    }
-]
+# Create your views here.
 
 
 def home(request):
-    return render(request, "base/home.html", {'rooms': rooms})
+    rooms = Room.objects.all()
+    return render(request, "base/home.html", {"rooms": rooms})
 
 
 def room(request, pk: str):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    return render(request, "base/room.html", {'room': room})
+    room = Room.objects.get(id=pk)
+    return render(request, "base/room.html", {"room": room})
